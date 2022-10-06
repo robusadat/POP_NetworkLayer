ae2d9bd109fdff6fffcb664432f8910536002c84
robusadat <sadathossain44@gmail.com>
2022-10-06 13:47:21 -0500

    Initial Commit

---
+++ POP_NetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POP_NetworkLayer
+//
+//  Created by Sadat on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ POP_NetworkLayerUITests/POP_NetworkLayerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  POP_NetworkLayerUITestsLaunchTests.swift
+//  POP_NetworkLayerUITests
+//
+//  Created by Sadat on 10/6/22.
+//
+
+import XCTest
+
+class POP_NetworkLayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
